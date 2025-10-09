45e94e92b24975e1b528e49d9e2c02882f1f1faf
sandeep_kumar <sandeeplavaniya7@gmail.com>
2025-10-09 19:14:07 +0530

    delete the Expense manager

--- ExpenseManager/src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Welcome to the Expense Manager!");
-
-        ExpenseManager expenseManager = new ExpenseManager();
-        expenseManager.demo();
-    }
-}
